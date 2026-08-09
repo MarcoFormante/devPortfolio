423e90f2877557af2a7ad8ce149ae760feb91b89
Marco <formante.marco@gmail.com>
2026-08-09 20:55:02 +0200

    add new div container with bg color and add max width 1920px  to sibling div container

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import Home from "./pages/home/Home";
 
 export default function App() {
   return (
-    <div className="flex">
+    <div className="bg-bg">
+      <div className="flex max-w-[1920px] m-auto">
         <Header/>
         <main className="flex-6">
             <Routes>
-               <Route index element={<Home/> } />
+              <Route index element={<Home/> } />
             </Routes>
         </main>
-        
+      </div>
     </div>
   )
 }
