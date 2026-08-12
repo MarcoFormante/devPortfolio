bfea891813cea8300bf127034c3451e8747b69fa
Marco <formante.marco@gmail.com>
2026-08-12 15:04:32 +0200

    add height to parent container and h-full to children containers

--- src/pages/home/About.tsx
+++ src/pages/home/About.tsx
@@ -25,8 +25,8 @@ const stackArray = [
 export default function About() {
   return (
     <section className="my-20" id="percorso">
-        <div className="flex gap-10">
-            <div className="p-[30px] px-10 bg-card-light flex flex-col gap-4 rounded-[34px] h-[320px] w-full flex-2">
+        <div className="flex gap-10 h-[340px]">
+            <div className="p-[30px] px-10 bg-card-light flex flex-col gap-4 rounded-[34px] h-full  w-full flex-2">
                 <div className="flex items-center gap-3"> 
                     <img
                         src={me}
@@ -43,7 +43,7 @@ export default function About() {
                 </p>   
             </div>
 
-            <div className="p-[30px] px-10 bg-surface flex flex-col gap-4 rounded-[34px] h-[320px] w-full flex-2 border border-text/10">
+            <div className="p-[30px] px-10 bg-surface flex flex-col gap-3 rounded-[34px] h-full w-full flex-2 border border-text/10">
                 <h2 className="text-text text-[34px] font-extrabold">Skill principali</h2>
                 <ul className="flex w-4/5 flex-wrap gap-2">
                     {stackArray.map(item => (
