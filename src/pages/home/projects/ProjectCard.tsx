5960e60aaa7869bedfd206caa0b2bcbfe4e75d13
Marco <formante.marco@gmail.com>
2026-08-12 15:00:19 +0200

    add textColor and bgColor types

--- src/pages/home/projects/ProjectCard.tsx
+++ src/pages/home/projects/ProjectCard.tsx
@@ -8,26 +8,29 @@ interface ProjectCardinterface{
     stack:string,
     desc:string,
     slug:string,
-    color:string
+    textColor:string,
+    bgColor:string
 }
 
 
 export default function ProjectCard({
-  index,
-  title,
-  stack,
-  desc,
-  slug,
-  color
+index,
+title,
+stack,
+desc,
+slug,
+textColor,
+bgColor
 }:ProjectCardinterface)
 {
    
 // IF index == NaN or index < 1 -> show "0" ELSE IF index < 10  -> show "0" + index  
 const formattedindex = (index > 0 && index < 10  ) ? "0" + index : isNaN(index) ? 0 : index; 
 
+
 return (
-    <article className={`border border-card-light/10 bg-surface rounded-[30px] flex flex-col gap-6 pt-6 pl-6 pb-8 max-h-[250px] relative after:w-[130px] after:h-[130px] after:absolute after:right-1/5 after:top-4 after:rounded-full after:blur-2xl after:bg-${color}`}>
-        <span className={`text-${color} text-cyan text-lg font-extrabold`}>{formattedindex}</span>
+    <article className={`border border-card-light/10 bg-surface rounded-[30px] flex flex-col gap-6 pt-6 pl-6 pb-8 max-h-[250px] relative after:w-[130px] after:h-[130px] after:absolute after:right-1/5 after:top-4 after:rounded-full after:blur-2xl ${bgColor}`}>
+        <span className={`${textColor} text-lg font-extrabold`}>{formattedindex}</span>
         <h3 className="text-text text-[28px]">{title}</h3>
         <div className="flex flex-col gap-3">
             <span className="text-muted font-extrabold text-[13px]">{stack}</span>
